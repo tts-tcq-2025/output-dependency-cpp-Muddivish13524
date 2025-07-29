a760c4b8709f2168fa3814afad6218738f19175c
Muddivish13524 <vishwanathmuddi@gmail.com>
2025-07-29 08:58:21 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 
-char size(int cms) {
-    char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
+// Returns the T-shirt size based on chest measurement in cms
+char getTshirtSize(int cms) {
+    if (cms < 38) {
+        return 'S';
+    } 
+    else if (cms <= 42) {   // fixed condition: include 38 and 42 in 'M' range
+        return 'M';
+    } 
+    else {
+        return 'L';
     }
-    return sizeName;
 }
 
 void testTshirtSize() {
-    std::cout << "\nTshirt size test\n";
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    std::cout << "\nT-shirt size test\n";
+    assert(getTshirtSize(37) == 'S');
+    assert(getTshirtSize(38) == 'M');  // added boundary test case
+    assert(getTshirtSize(40) == 'M');
+    assert(getTshirtSize(42) == 'M');  // added boundary test case
+    assert(getTshirtSize(43) == 'L');
     std::cout << "All is well (maybe!)\n";
+}
+
+int main() {
+    testTshirtSize();
+    return 0;
 }
